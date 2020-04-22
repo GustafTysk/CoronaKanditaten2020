3dba049e8becdb5e8f032ebf69724e89ba2c40ab
Tjalexander97 <alewik97@gmail.com>
2020-04-22 14:37:41 +0200

    Graphs in statistics window.

--- app/src/main/java/com/example/coronakanditaten2020/StatisticsFragment.java
+++ app/src/main/java/com/example/coronakanditaten2020/StatisticsFragment.java
@@ -1,6 +1,6 @@
 package com.example.coronakanditaten2020;
 
-import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,8 +13,10 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GridLabelRenderer;
+import com.jjoe64.graphview.ValueDependentColor;
+import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
-import com.jjoe64.graphview.series.LineGraphSeries;
 
 
 public class StatisticsFragment extends Fragment {
@@ -22,6 +24,7 @@ public class StatisticsFragment extends Fragment {
 
     private Button btnStatisticsToStart;
     private Button btnStatisticsToHeatmap;
+    private Location[] Locations = new Location[12];
 
 
     @Nullable
@@ -31,21 +34,127 @@ public class StatisticsFragment extends Fragment {
         btnStatisticsToStart = (Button) view.findViewById(R.id.btnStatisticsToStart);
         btnStatisticsToHeatmap = (Button) view.findViewById(R.id.btnStatisticsToHeatmap);
 
-        GraphView graph = (GraphView) view.findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6)
+        Locations[0] =new Location("59.858562", "17.638927", "2020-10-15", 0, 0, 2, 0, 3, 0, 1, 1, 0, 2, "2020-10-17 10:30");
+        Locations[1] =new Location("59.858562", "17.638927", "2020-10-15", 1, 2, 3, 0, 2, 1, 1, 2, 1, 1, "2020-10-17 10:30");
+        Locations[2] =new Location("59.858562", "17.638927", "2020-10-15", 2, 2, 1, 0, 3, 2, 1, 3, 2, 2, "2020-10-17 10:30");
+        Locations[3] =new Location("59.858562", "17.638927", "2020-10-15", 3, 3, 2, 0, 1, 3, 1, 1, 0, 3, "2020-10-17 10:30");
+        Locations[4] =new Location("59.858562", "17.638927", "2020-10-15", 4, 0, 0, 3, 3, 2, 1, 2, 0, 2, "2020-10-17 10:30");
+        Locations[5] =new Location("59.858562", "17.638927", "2020-10-15", 5, 0, 3, 0, 2, 2, 1, 3, 1, 1, "2020-10-17 10:30");
+        Locations[6] =new Location("59.858562", "17.638927", "2020-10-15", 6, 2, 0, 1, 3, 1, 1, 1, 0, 0, "2020-10-17 10:30");
+        Locations[7] =new Location("59.858562", "17.638927", "2020-10-15", 7, 3, 0, 2, 1, 2, 1, 2, 0, 3, "2020-10-17 10:30");
+        Locations[8] =new Location("59.858562", "17.638927", "2020-10-15", 8, 0, 0, 2, 3, 1, 1, 3, 3, 2, "2020-10-17 10:30");
+        Locations[9] =new Location("59.858562", "17.638927", "2020-10-15", 9, 1, 2, 0, 0, 0, 1, 1, 0, 1, "2020-10-17 10:30");
+        Locations[10] =new Location("59.858562", "17.638927", "2020-10-15", 10, 2, 1, 0, 3, 3, 1, 1, 0, 2, "2020-10-17 10:30");
+        Locations[11] =new Location("59.858562", "17.638927", "2020-10-15", 11, 3, 2, 0, 1, 0, 1, 1, 1, 3, "2020-10-17 10:30");
+
+        //DIARRHEA
+        int zeros = 0;
+        int ones = 0;
+        int twos = 0;
+        int threes = 0;
+        for(int i=0; i<Locations.length; i++) {
+            if (Locations[i].getDiarrheaRatingBar() == 0) {
+                zeros += 1;
+            } else if (Locations[i].getDiarrheaRatingBar() == 1) {
+                ones += 1;
+            } else if(Locations[i].getDiarrheaRatingBar() == 2) {
+                twos += 1;
+            } else if(Locations[i].getDiarrheaRatingBar() == 3) {
+                threes += 1;
+            }
+        }
+        GraphView graph = (GraphView) view.findViewById(R.id.graph1);
+        BarGraphSeries<DataPoint> series = new BarGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, zeros),
+                new DataPoint(1, ones),
+                new DataPoint(2, twos),
+                new DataPoint(3, threes),
         });
         graph.addSeries(series);
-        // activate horizontal zooming and scrolling
-        graph.getViewport().setScalable(true);
-        graph.setTitle("Magic");
-        graph.setTitleTextSize(100);
-        //graph.setTitleColor();
-        series.setDrawDataPoints(true);
+        series.setValueDependentColor(new ValueDependentColor<DataPoint>() {
+            @Override
+            public int get(DataPoint data) {
+                return Color.rgb((int) data.getX()*255/4, (int) Math.abs(data.getY()*255/6), 100);
+            }
+        });
+
+        series.setSpacing(50);
+        series.setDrawValuesOnTop(true);                // draw values on top of staplar
+        series.setValuesOnTopColor(Color.RED);
+        series.setValuesOnTopSize(40);
+        //graph.getViewport().setScalable(true);        // activate horizontal zooming and scrolling
+        graph.setTitle("Diarrhea");
+        graph.setTitleTextSize(80);
+        //graph.setTitleColor(Color.BLUE);
+        //graph.getLegendRenderer().setVisible(true);
+        graph.getViewport().setMinY(0);
+        graph.getViewport().setMaxY(6);
+        graph.getViewport().setMinX(0);
+        graph.getViewport().setMaxX(4);
+        graph.getViewport().setYAxisBoundsManual(true);
+        graph.getViewport().setXAxisBoundsManual(true);
+        GridLabelRenderer gridLabelX = graph.getGridLabelRenderer();
+        gridLabelX.setHorizontalAxisTitle("Severity of symptom");
+        GridLabelRenderer gridLabelY = graph.getGridLabelRenderer();
+        gridLabelY.setVerticalAxisTitle("Number of people");
+
+        //ALLA SYMPTOM
+
+//FEVER
+
+        int feverZeros = 0;
+        int feverOnes = 0;
+        int feverTwos = 0;
+        int feverThrees = 0;
+        for(int i=0; i<Locations.length; i++) {
+            if (Locations[i].getFeverRatingBar() == 0) {
+                feverZeros += 1;
+            } else if (Locations[i].getFeverRatingBar() == 1) {
+                feverOnes += 1;
+            } else if(Locations[i].getFeverRatingBar() == 2) {
+                feverTwos += 1;
+            } else if(Locations[i].getFeverRatingBar() == 3) {
+                feverThrees += 1;
+            }
+        }
+        GraphView graph2 = (GraphView) view.findViewById(R.id.graph2);
+        BarGraphSeries<DataPoint> series2 = new BarGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, feverZeros),
+                new DataPoint(1, feverOnes),
+                new DataPoint(2, feverTwos),
+                new DataPoint(3, feverThrees),
+        });
+
+        graph2.addSeries(series2);
+        series2.setValueDependentColor(new ValueDependentColor<DataPoint>() {
+            @Override
+            public int get(DataPoint data) {
+                return Color.rgb((int) data.getX()*255/4, (int) Math.abs(data.getY()*255/6), 100);
+            }
+        });
+        series2.setSpacing(50);
+        series2.setDrawValuesOnTop(true);                // draw values on top of staplar
+        series2.setValuesOnTopColor(Color.RED);
+        series2.setValuesOnTopSize(40);
+//graph.getViewport().setScalable(true);        // activate horizontal zooming and scrolling
+        graph2.setTitle("Fever");
+        graph2.setTitleTextSize(80);
+//graph.setTitleColor(Color.BLUE);
+//graph.getLegendRenderer().setVisible(true);
+        graph2.getViewport().setMinY(0);
+        graph2.getViewport().setMaxY(8);
+        graph2.getViewport().setMinX(0);
+        graph2.getViewport().setMaxX(4);
+        graph2.getViewport().setYAxisBoundsManual(true);
+        graph2.getViewport().setXAxisBoundsManual(true);
+        GridLabelRenderer gridLabelX2 = graph2.getGridLabelRenderer();
+        gridLabelX2.setHorizontalAxisTitle("Severity of symptom");
+        GridLabelRenderer gridLabelY2 = graph2.getGridLabelRenderer();
+        gridLabelY2.setVerticalAxisTitle("Number of people");
+
+
+
+
 
         btnStatisticsToStart.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,6 +169,8 @@ public class StatisticsFragment extends Fragment {
                 Toast.makeText(getActivity(), "Going to Heatmap", Toast.LENGTH_SHORT).show();
                 ((MainActivity)getActivity()).setViewPager(2);
             }
+
+
         });
 
         return view;
