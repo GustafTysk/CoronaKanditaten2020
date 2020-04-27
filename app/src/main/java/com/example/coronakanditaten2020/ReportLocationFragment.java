8533145268ea7cf59d562fe63cfad37b3b2385ae
FelixArnestrand <felix.arnestrand.uu@gmail.com>
2020-04-27 11:39:09 +0200

    Revert

--- app/src/main/java/com/example/coronakanditaten2020/ReportLocationFragment.java
+++ app/src/main/java/com/example/coronakanditaten2020/ReportLocationFragment.java
@@ -1,35 +1,48 @@
 package com.example.coronakanditaten2020;
 
+import androidx.annotation.Dimension;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.media.Image;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.PopupWindow;
 import android.widget.Toast;
 
+import com.google.android.gms.common.util.JsonUtils;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.TileOverlayOptions;
 import com.google.maps.android.heatmaps.HeatmapTileProvider;
 
 public class ReportLocationFragment extends Fragment  implements OnMapReadyCallback, View.OnClickListener{
     private static final String TAG = "Fragment Statistics";
+
+
     GoogleMap mGoogleMap;
     MapView mMapView;
+
+    Bundle savedInstance;
 
     private Button btnRlToStart;
     private Button btnRlToRs;
@@ -39,6 +52,7 @@ public class ReportLocationFragment extends Fragment  implements OnMapReadyCallb
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        savedInstance = savedInstanceState;
         View view = inflater.inflate(R.layout.fragment_report_location, container, false);
         btnRlToStart = (Button) view.findViewById(R.id.btnRlToStart);
         btnRlToStart.setOnClickListener(this);
@@ -46,7 +60,8 @@ public class ReportLocationFragment extends Fragment  implements OnMapReadyCallb
         btnRlToRs.setOnClickListener(this);
 
         setLocation1 = (ImageButton) view.findViewById(R.id.setLocation1);
-        //setLocation1.setOnClickListener(this);
+        setLocation1.setOnClickListener(this);
+
 
 
 
@@ -67,36 +82,41 @@ public class ReportLocationFragment extends Fragment  implements OnMapReadyCallb
                 ((MainActivity) getActivity()).setViewPager(0);
                 break;
             case R.id.setLocation1:
+
                 LayoutInflater inflater = (LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
                 System.out.println("Kommer in hit");
                 View mapV = inflater.inflate(R.layout.report_location_map, null);
-                final PopupWindow mapWindow = new PopupWindow(mapV, 400, 400);
-                mapWindow.showAtLocation(v, Gravity.CENTER, 0, 0);
-                mMapView = (MapView) mapV.findViewById(R.id.mapViewReport);
-                //mMapView.onCreate(savedInstanceState);
-                mMapView.getMapAsync(new OnMapReadyCallback() {
-                    @Override
-                    public void onMapReady(GoogleMap googleMap) {
-                        mGoogleMap = googleMap;
-                        mGoogleMap.getUiSettings().setZoomControlsEnabled(true);
-                        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(59.8, 17.63), 10));
-                        System.out.println("kkk");
 
-                    }
-                });
+                mMapView = (MapView) mapV.findViewById(R.id.mapViewReport);
+                mMapView.onCreate(savedInstance);
+
+                mMapView.getMapAsync(this);
+                final PopupWindow mapWindow = new PopupWindow(mapV, WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT);
+                mapWindow.showAtLocation(v, Gravity.CENTER, 0, 0);
+
+                mapWindow.setOutsideTouchable(true);
+                mapWindow.setFocusable(true);
+                mapWindow.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+
+
         }
     }
-
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        System.out.println("jajaj");
+        mGoogleMap = googleMap;
+        mGoogleMap.getUiSettings().setZoomControlsEnabled(true);
+        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(59.8, 17.63), 10));
+        System.out.println("kkk");
 
     }
+
 
     @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        mMapView.onSaveInstanceState(outState);
+    public void onDestroy() {
+        super.onDestroy();
+        mMapView.onDestroy();
     }
+
 }
