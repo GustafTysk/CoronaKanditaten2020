4bcb70a33b96d63d8f1b816f10182dc36f12e720
Michiel <michieln98@gmail.com>
2020-05-08 15:07:04 +0200

    Forum update

--- app/src/main/java/com/example/coronakanditaten2020/ForumFragment.java
+++ app/src/main/java/com/example/coronakanditaten2020/ForumFragment.java
@@ -1,6 +1,8 @@
 package com.example.coronakanditaten2020;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,9 +27,11 @@ public class ForumFragment extends Fragment implements View.OnClickListener {
     private Button messageButton;
     private Button btnForumToStart;
 
+    private EditText searchFilter;
+
     private EditText messageInput;
     private EditText messageTitle;
-    private TextView messageShow;
+
     private String currentCategory;
 
     public ListView listView;
@@ -36,8 +40,7 @@ public class ForumFragment extends Fragment implements View.OnClickListener {
     public int parentId = 0;
 
     public ArrayList<Post>postList;
-
-
+    private PostListAdapter adapter;
 
     @Nullable
     @Override
@@ -50,49 +53,61 @@ public class ForumFragment extends Fragment implements View.OnClickListener {
         btnForumToStart.setOnClickListener(this);
         messageInput = (EditText) view.findViewById(R.id.messageInput);
         messageTitle = (EditText) view.findViewById(R.id.messageTitle);
-        messageShow = (TextView) view.findViewById(R.id.messageShow);
 
+        searchFilter = (EditText) view.findViewById(R.id.searchFilter);
         listView = (ListView) view.findViewById(R.id.listview);
         ArrayList<String> likes = new ArrayList<String>();
+
         likes.add("1");
         currentCategory = "Help";
 
         Post newPost = new Post("Person1", "title1", "23 jan", "I need help", likes, "help", 1, 0);
         Post newPost2 = new Post("Person2", "title2", "24 jan", "Me too", likes, "help", 2, 0);
         Post newPost3 = new Post("Person3", "title3", "25 jan", "Me same", likes, "help",3, 0);
+        Post newPost4 = new Post("Person4", "title4", "26 jan", "I need help", likes, "help", 4, 0);
+        Post newPost5 = new Post("Person5", "title5", "27 jan", "Me too", likes, "help", 5, 0);
+        Post newPost6 = new Post("Person6", "title6", "28 jan", "Me same", likes, "help",6, 0);
+        Post newPost7 = new Post("Person7", "title7", "29 jan", "I need help", likes, "help", 7, 0);
+        Post newPost8 = new Post("Person8", "title8", "30 jan", "Me too", likes, "help", 8, 0);
+        Post newPost9 = new Post("Person9", "title9", "31 jan", "Me same", likes, "help",9, 0);
 
         postList = new ArrayList<>();
 
         postList.add(newPost);
         postList.add(newPost2);
         postList.add(newPost3);
+        postList.add(newPost4);
+        postList.add(newPost5);
+        postList.add(newPost6);
+        postList.add(newPost7);
+        postList.add(newPost8);
+        postList.add(newPost9);
 
 
-        PostListAdapter adapter = new PostListAdapter(getContext(), R.layout.adapter_view_layout, postList);
+        adapter = new PostListAdapter(getContext(), R.layout.adapter_view_layout, postList);
         listView.setAdapter(adapter);
+
+        searchFilter.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                (ForumFragment.this).adapter.getFilter().filter(s);
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
 
         return view;
     }
-
-    public void onRadioButtonClicked(View view) {
-        // Is the button now checked?
-        boolean checked = ((RadioButton) view).isChecked();
-
-        // Check which radio button was clicked
-        switch(view.getId()) {
-            case R.id.radio_pirates:
-                if (checked)
-                    // Pirates are the best
-                    break;
-            case R.id.radio_ninjas:
-                if (checked)
-                    // Ninjas rule
-                    break;
-        }
-    }
-
-
 
     @Override
     public void onClick(View v) {
@@ -105,12 +120,11 @@ public class ForumFragment extends Fragment implements View.OnClickListener {
                 String message = messageInput.getText().toString();
                 ArrayList<String> likes = new ArrayList<String>();
                 likes.add("1");
-                // messageShow.setText(message);
-                // messageShow.append(title + "\n" + message + "\n");
 
-                Post newWrittenPost = new Post(username, title, timestamp, message, likes, currentCategory, id, parentId);
+                Post newWrittenPost = new Post(username, title, timestamp, message, likes, "help", 30, 0);
                 System.out.println(newWrittenPost.printInformation());
                 postList.add(newWrittenPost);
+                adapter.notifyDataSetChanged();
                 break;
             case R.id.btnForumToStart:
                 ((MainActivity) getActivity()).setViewPager(0);
--- app/src/main/java/com/example/coronakanditaten2020/PostListAdapter.java
+++ app/src/main/java/com/example/coronakanditaten2020/PostListAdapter.java
@@ -1,14 +1,21 @@
 package com.example.coronakanditaten2020;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,6 +25,15 @@ public class PostListAdapter extends ArrayAdapter<Post> {
     private static final String tag = "PostListAdapter";
     private Context mContext;
     private int mResource;
+    private int lastPostion = -1;
+
+    static class ViewHolder {
+        TextView username;
+        TextView title;
+        TextView text;
+        TextView likesShow;
+
+    }
 
     public PostListAdapter(Context context, int resource, ArrayList<Post> objects) {
         super(context, resource, objects);
@@ -39,16 +55,40 @@ public class PostListAdapter extends ArrayAdapter<Post> {
 
         Post post = new Post(username, title, timestamp, text, likes, category, id, parentId);
 
-        LayoutInflater inflater = LayoutInflater.from(mContext);
-        convertView = inflater.inflate(mResource, parent, false);
+        final View result;
+        ViewHolder holder;
 
-        TextView postTitle = (TextView) convertView.findViewById(R.id.postTitle);
-        TextView postMessage = (TextView) convertView.findViewById(R.id.postMessage);
-        TextView postCategory = (TextView) convertView.findViewById(R.id.postCategory);
+        if (convertView==null) {
+            LayoutInflater inflater = LayoutInflater.from(mContext);
+            convertView = inflater.inflate(mResource, parent, false);
+            holder = new ViewHolder();
+            holder.username = (TextView) convertView.findViewById(R.id.postUsername);
+            holder.title = (TextView) convertView.findViewById(R.id.postTitle);
+            holder.text = (TextView) convertView.findViewById(R.id.postMessage);
+            holder.likesShow = (TextView) convertView.findViewById(R.id.postLikes);
 
-        postTitle.setText(title);
-        postMessage.setText(text);
-        postCategory.setText(category);
+            result = convertView;
+
+            convertView.setTag(holder);
+        }
+        else{
+            holder = (ViewHolder) convertView.getTag();
+            result = convertView;
+        }
+
+
+        Animation animation = AnimationUtils.loadAnimation(mContext,
+                (position > lastPostion) ? R.anim.load_down_anim : R.anim.load_up_anim);
+        result.startAnimation(animation);
+        lastPostion = position;
+
+        holder.username.setText(username);
+        holder.title.setText(title);
+        holder.text.setText(text);
+//        for(int i = 0; i < likes.length ; i++){
+//            Log.d("string is",(String)likes[i]);
+//        }
+        holder.likesShow.setText("Likes: " + likes.get(0));
 
         return convertView;
 
