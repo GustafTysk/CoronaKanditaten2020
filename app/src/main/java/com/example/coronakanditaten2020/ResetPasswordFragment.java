b376cf87db60b5134667d8fd77b59a83e40ab058
Felix Arnestrand <felix.arnestrand.uu@gmail.com>
2020-05-12 10:44:46 +0200

    quick fix forum + reset password fragment beginning

---
+++ app/src/main/java/com/example/coronakanditaten2020/ResetPasswordFragment.java
@@ -0,0 +1,25 @@
+package com.example.coronakanditaten2020;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class ResetPasswordFragment extends Fragment implements View.OnClickListener {
+
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_reset_password, container, false);
+
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
